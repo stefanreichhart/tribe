179b64a50ba48cb998d61357a22363959ce18e4c
Stefan Reichhart <stefan.reichhart@gmail.com>
2016-09-25 21:16:55 +0200

    Start and stop scripts

--- src/run.sh
+++ src/run.sh
@@ -3,7 +3,7 @@
 echo "Checking node.js ..."
 VERSION_NPM=`npm --version 2> /dev/null`
 if [ -z "$VERSION_NPM" ]; then 
-	echo "Please install node.js first"
+	echo "ERROR: Please install node.js first"
 	echo " ---> https://nodejs.org/en/download/)"
 	exit 1
 else
@@ -14,7 +14,7 @@ fi
 echo "Checking bower ..."
 VERSION_BOWER=`bower --version 2> /dev/null`
 if [ -z "$VERSION_BOWER" ]; then 
-	echo "Please install bower first"
+	echo "ERROR: Please install bower first"
 	echo " ---> sudo npm install -g bower"
 	exit 1
 else
@@ -23,12 +23,21 @@ fi
 
 COUNT_PROCESS=`ps -Aef | grep node | grep -c server.js`
 if [ $COUNT_PROCESS -gt 0 ]; then
-	echo "This project is already running ... "
+	echo "INFO: This project is already running ... "
 	echo "Processes: $COUNT_PROCESS"
 	exit 0
 else
 	echo "Starting up ... "
-	npm start
+	npm start &
 fi
 
+COUNT_PROCESS=0
+while [ $COUNT_PROCESS -eq 0 ]
+do
+	echo "Waiting on server to start ..."
+	sleep 0.25
+	COUNT_PROCESS=`ps -Aef | grep node | grep -c server.js`
+done
 
+echo "INFO: Server started"
+echo " ---> http://localhost:8888"
